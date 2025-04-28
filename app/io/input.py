a10b3d3bcb3ea590d3f2e28a22f5f7468e491464
Vladyslav Semchuk <semchukvlad05@gmail.com>
2025-04-28 12:51:55 +0300

    Added empty files into output and input

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+def input_text_from_console():
+    """Функція для вводу тексту з консолі."""
+    pass
+
+
+def read_from_file_builtin(filename):
+    """Функція для зчитування тексту з файлу через вбудовані можливості Python."""
+    pass
+
+
+def read_from_file_pandas(filename):
+    """Функція для зчитування даних з файлу за допомогою бібліотеки pandas."""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def output_text_to_console(text):
+    """Функція для виведення тексту в консоль."""
+    pass
+
+
+def write_text_to_file(filename, text):
+    """Функція для запису тексту у файл через вбудовані можливості Python."""
+    pass
